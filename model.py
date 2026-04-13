fa8471c70e23f537c9e688283454fd93f85aef23
Chandu796 <chanducm2006@gmail.com>
2026-04-13 12:46:47 +0530

    improved random forest with hyperparameters

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 def split_data(X, y):
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
