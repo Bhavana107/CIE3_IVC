166f84d54a1965dc940a84c465df08b6f39aed5e
Chandu796 <chanducm2006@gmail.com>
2026-04-13 12:46:10 +0530

    added data splitting and random forest model

---
+++ model.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+def split_data(X, y):
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def train_random_forest(X_train, y_train):
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
+
+def train_svm(X_train, y_train):
+    model = SVC()
+    model.fit(X_train, y_train)
+    return model
+
+def predict(model, X_test):
+    return model.predict(X_test)
